63fe421e00d2fb2fd7cb8c94a4e7c88024653208
ShinyaKato <k.shin220@gmail.com>
2019-05-19 04:13:40 +0900

    Improve source code

--- solve.cpp
+++ solve.cpp
@@ -6,6 +6,10 @@ using namespace std;
 
 const int INF = 1 << 28;
 
+#ifndef BUDGET
+#define BUDGET 1000
+#endif
+
 int main(void) {
   int n;
   cin >> n;
@@ -18,19 +22,25 @@ int main(void) {
     cin >> id >> name[i] >> category >> type >> price[i] >> calorie[i] >> salt;
   }
 
-  vector<vector<int>> dp(n + 1, vector<int>(1001, -INF));
+  vector<vector<int>> dp(n + 1, vector<int>(BUDGET + 1, -INF));
   dp[0][0] = 0;
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j <= 1000; j++) {
+    for (int j = 0; j <= BUDGET; j++) {
       dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
+#if !DUPLICATE
       if (j - price[i] >= 0) {
         dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - price[i]] + calorie[i]);
       }
+#else
+      if (j - price[i] >= 0) {
+        dp[i + 1][j] = max(dp[i + 1][j], dp[i + 1][j - price[i]] + calorie[i]);
+      }
+#endif
     }
   }
 
   int best_price = 0, best_calorie = 0;
-  for (int i = 0; i <= 1000; i++) {
+  for (int i = 0; i <= BUDGET; i++) {
     if (best_calorie < dp[n][i]) {
       best_price = i;
       best_calorie = dp[n][i];
@@ -38,20 +48,31 @@ int main(void) {
   }
 
   vector<int> selected;
-  for (int i = n - 1, j = best_price; i != 0 || j != 0;) {
+  for (int i = n - 1, j = best_price; i != -1 || j != 0;) {
     assert(i >= 0 && j >= 0);
     if (dp[i][j] == dp[i + 1][j]) {
       i--;
-    } else if (j - price[i] >= 0 && dp[i][j - price[i]] + calorie[i] == dp[i + 1][j]) {
+      continue;
+    }
+#if !DUPLICATE
+    if (j - price[i] >= 0 && dp[i][j - price[i]] + calorie[i] == dp[i + 1][j]) {
       selected.push_back(i);
       j -= price[i];
       i--;
-    } else {
-      assert(false);
+      continue;
     }
+#else
+    if (j - price[i] >= 0 && dp[i + 1][j - price[i]] + calorie[i] == dp[i + 1][j]) {
+      selected.push_back(i);
+      j -= price[i];
+      continue;
+    }
+#endif
+    assert(false);
   }
   reverse(selected.begin(), selected.end());
 
+  cout << "予算 = " << BUDGET << endl;
   for (int i : selected) {
     cout << name[i] << ": " << price[i] << "円, " << calorie[i] << " kcal" << endl;
   }
